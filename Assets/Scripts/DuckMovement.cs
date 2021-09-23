bdf3a5f0ac5a1780e5b5f439afe00d35d3e40956
sinash <pine0113@gmail.com>
2021-09-24 00:15:25 +0800

    add duck movement

---
+++ Assets/Scripts/DuckMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class DuckMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+      
+
+        anim.SetBool("run", (horizontalInput !=0) );
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
